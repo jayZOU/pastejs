e5d90a9b0516687495031ee928ac5a1301eb6f34
shijiezou(邹世杰) <shijiezou@tencent.com>
2016-08-12 16:33:53 +0800

    only support chrome

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # pastejs
 
 
-> 从剪切板中粘贴图片到DOM上
+> 从剪切板中粘贴图片到DOM上，仅支持chrome浏览器
+
+[demo][1]
 
 ---
 ## install
@@ -13,19 +15,17 @@
     
     const Pastejs = require('Pastejs');
     const pastejs = new Pastejs({
-    		isDebug: true,
-    		target : box,                                   //剪切区域(ID)
-    		callBack: function(paste){                      //回调，返回blob和剪切图片属性
-    			console.log(paste);
-    			document.getElementById("img").src = paste['source'];
-    			document.getElementById("text").innerHTML = "size:" + paste['blob'].size + "<br>type:" + paste['blob'].type;
-    		}
-    	});
+            isDebug: true,
+            target : box,                                   //剪切区域(ID)
+            callBack: function(paste){                      //回调，返回blob和剪切图片属性
+                console.log(paste);
+                document.getElementById("img").src = paste['source'];
+                document.getElementById("text").innerHTML = "size:" + paste['blob'].size + "<br>type:" + paste['blob'].type;
+            }
+        });
 ---
 ## License
 MIT
 
 
-
-
-
+  [1]: http://jayzou.github.io/pastejs/index.html
--- src/pastejs.js
+++ src/pastejs.js
@@ -1,49 +1,57 @@
 class Pastejs {
-	constructor(opts) {
-		this.isDebug = opts.isDebug || false;
-		this.target = opts.target || null;
-		this.callBack = opts.callBack || function(){};
-		
-		this._init();
-	}
-	_init() {
-		let self = this,
-			target = self.target;
-		if(!target) this.throwIf('必须传入note参数');
+    constructor(opts) {
+        this.isDebug = opts.isDebug || false;
+        this.target = opts.target || null;
+        this.callBack = opts.callBack || function() {};
 
-		self._bindEvent();
-	}
-	_bindEvent() {
-		let self = this,
-			target = self.target;
-		target.addEventListener("paste", function(e){
-			if (!(e.clipboardData && e.clipboardData.items && e.clipboardData.items[0])){
-                this.throwIf('不支持剪切板事件');
-                return ;
+        this._init();
+    }
+    _init() {
+        let self = this,
+            target = self.target;
+        if (!target) self.throwIf('必须传入note参数');
+        if (!self._isBrowser()) {
+        	self.throwIf('该浏览器不支持剪切板上传，请换chrome');
+        	return;
+        }
+        // self._isBrowser();
+        self._bindEvent();
+    }
+    _isBrowser() {
+        return navigator.userAgent.toLowerCase().match(/chrome\/([\d.]+)/);
+    }
+    _bindEvent() {
+        let self = this,
+            target = self.target;
+
+        target.addEventListener("paste", function(e) {
+            if (!(e.clipboardData && e.clipboardData.items && e.clipboardData.items[0])) {
+                self.throwIf('不能从本地直接复制文件');
+                return;
             }
             let items = e.clipboardData.items,
-            	paste = [];
-            	if (items) {
-		            for (var i = 0; i < items.length; i++) {
-		                if (items[i].type.indexOf("image") !== -1) {
-		                    let blob = items[i].getAsFile();
-		                    let URLObj = window.URL || window.webkitURL || window.createObjectURL;
-		                    let source = URLObj.createObjectURL(blob);
-		                    paste['blob'] = blob;
-		                    paste['source'] = source;
-		                    self.callBack(paste)
-		                }
-		            }
-		        }
-		});
-	}
-	//错误数据弹出
-	throwIf(msg = '未知错误') {
-		if(this.isDebug){
-			alert(msg);
-			return;
-		}
-	}
+                paste = [];
+            if (items) {
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].type.indexOf("image") !== -1) {
+                        let blob = items[i].getAsFile();
+                        let URLObj = window.URL || window.webkitURL || window.createObjectURL;
+                        let source = URLObj.createObjectURL(blob);
+                        paste['blob'] = blob;
+                        paste['source'] = source;
+                        self.callBack(paste)
+                    }
+                }
+            }
+        });
+    }
+    //错误数据弹出
+    throwIf(msg = '未知错误') {
+        if (this.isDebug) {
+            alert(msg);
+            return;
+        }
+    }
 }
 
 module.exports = Pastejs;
